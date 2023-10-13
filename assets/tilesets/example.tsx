48b91e314a2ea4f7d950c5b571bf48a5646e5e49
Patrick Canfield <pscale01@gmail.com>
2023-10-13 09:15:35 -0700

    design level

---
+++ assets/tilesets/example.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="kenny_simple_platformer" tilewidth="64" tileheight="64" tilecount="98" columns="14">
+ <image source="platformPack_tilesheet.png" width="896" height="448"/>
+</tileset>
