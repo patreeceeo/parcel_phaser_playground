2adc95f238fb98924c965f54351f6c2e3e191722
Patrick Canfield <pscale01@gmail.com>
2023-10-14 09:57:52 -0700

    flip player

--- src/app.ts
+++ src/app.ts
@@ -86,6 +86,12 @@ class MyScene extends Phaser.Scene {
       this.#player!.setVelocityY(-350);
       this.#player!.play('jump', true);
     }
+    if (this.#player!.body.velocity.x > 0) {
+      this.#player!.setFlipX(false);
+    } else if (this.#player!.body.velocity.x < 0) {
+      // otherwise, make them face the other side
+      this.#player!.setFlipX(true);
+    }
   }
 }
 
