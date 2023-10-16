c9c3ed9984e13ba0410c5fddff1194961a571344
Patrick Canfield <pscale01@gmail.com>
2023-10-16 13:57:14 -0700

    replace my module.hot dts with that of DefinitelyTyped

--- types/hmr.d.ts
+++
@@ -1,9 +0,0 @@
-interface ESModule<D extends Record<string, any>> {
-  hot: {
-    dispose: (callback: (data: D) => void) => void;
-    accept: (callback: () => void) => void;
-    data: D
-  };
-}
-
-declare const module: ESModule;
